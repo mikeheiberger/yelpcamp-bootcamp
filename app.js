5b5b59537dc4fa0e95eb39ddd7e1424cc7d7ecc1
Mike Heiberger <michael.heiberger@gmail.com>
2018-04-17 15:05:01 -0400

    Added a local default db URL

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const commentRoutes = require('./routes/comments');
 const campgroundRoutes = require('./routes/campgrounds');
 const indexRoutes = require('./routes/index');
 
-mongoose.connect(process.env.DATABASEURL);
+var dbUrl = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(dbUrl);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
